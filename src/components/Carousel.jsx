99af1c47dfceb748781e5ba59fb7a8f4c967db37
Bantyb792 <bantyb792@gmail.com>
2025-04-29 20:43:05 +0530

    Utilized updater function to set state values

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,32 +7,36 @@ import { Button } from "neetoui";
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const handleNext = () => {
-    setCurrentIndex((currentIndex + 1) % imageUrls.length);
+    setCurrentIndex(currentIndex => (currentIndex + 1) % imageUrls.length);
   };
 
   const handlPrevious = () => {
-    setCurrentIndex((currentIndex - 1 + imageUrls.length) % imageUrls.length);
+    setCurrentIndex(
+      currentIndex => (currentIndex - 1 + imageUrls.length) % imageUrls.length
+    );
   };
 
   return (
     <div className="flex flex-col items-center">
-      <Button
-        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-        icon={Left}
-        style="text"
-        onClick={handlPrevious}
-      />
-      <img
-        alt={title}
-        className="max-w-56 h-56 max-h-56 w-56"
-        src={imageUrls[currentIndex]}
-      />
-      <Button
-        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-        icon={Right}
-        style="text"
-        onClick={handleNext}
-      />
+      <div className="flex items-center">
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Left}
+          style="text"
+          onClick={handlPrevious}
+        />
+        <img
+          alt={title}
+          className="max-w-56 h-56 max-h-56 w-56"
+          src={imageUrls[currentIndex]}
+        />
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Right}
+          style="text"
+          onClick={handleNext}
+        />
+      </div>
       <div className="flex space-x-1">
         {imageUrls.map((_, index) => (
           <span
