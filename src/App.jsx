13b8a447216205392409251735c0c8e371655790
Bantyb792 <bantyb792@gmail.com>
2025-04-30 17:51:54 +0530

    Standardized frontend routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,19 @@
-import PageNotFound from "components/PageNotFound";
-import { NavLink, Route, Switch } from "react-router-dom";
+import { PageNotFound } from "components/commons";
+import { Redirect, Route, Switch } from "react-router-dom";
+import routes from "routes";
 
 import "./App.css";
-import Home from "./components/Home";
 import Product from "./components/Product";
+import ProductList from "./components/ProductList";
 
 // eslint-disable-next-line import/extensions
 
 const App = () => (
-  <>
-    <div className="flex space-x-2">
-      <NavLink exact activeClassName="underline font-bold" to="/">
-        Home
-      </NavLink>
-      <NavLink exact activeClassName="underline font-bold" to="/product">
-        Product
-      </NavLink>
-    </div>
-    <Switch>
-      <Route exact component={Product} path="/product" />
-      <Route exact component={Home} path="/" />
-      <Route exact component={PageNotFound} path="/*" />
-    </Switch>
-  </>
+  <Switch>
+    <Route exact component={Product} path={routes.products.show} />
+    <Route exact component={ProductList} path={routes.products.index} />
+    <Redirect exact from={routes.root} to={routes.products.index} />
+    <Route exact component={PageNotFound} path="*" />
+  </Switch>
 );
 export default App;
