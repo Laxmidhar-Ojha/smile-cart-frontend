1625ec6892902799c58829584671d72dfb847b04
Bantyb792 <bantyb792@gmail.com>
2025-04-30 10:22:18 +0530

    Intefrated API to display product details

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import classNames from "classnames";
 import { Left, Right } from "neetoicons";
@@ -6,6 +6,8 @@ import { Button } from "neetoui";
 
 const Carousel = ({ imageUrls, title }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const timerRef = useRef(null);
+
   const handleNext = () => {
     setCurrentIndex(currentIndex => (currentIndex + 1) % imageUrls.length);
   };
@@ -14,13 +16,19 @@ const Carousel = ({ imageUrls, title }) => {
     setCurrentIndex(
       currentIndex => (currentIndex - 1 + imageUrls.length) % imageUrls.length
     );
+    resetTimer();
   };
 
   useEffect(() => {
-    const interval = setInterval(handleNext, 3000);
+    timerRef.current = setInterval(handleNext, 3000);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(timerRef.current);
   }, []);
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    timerRef.current = setInterval(handleNext, 3000);
+  };
 
   return (
     <div className="flex flex-col items-center">
@@ -40,7 +48,10 @@ const Carousel = ({ imageUrls, title }) => {
           className="shrink-0 focus-within:ring-0 hover:bg-transparent"
           icon={Right}
           style="text"
-          onClick={handleNext}
+          onClick={() => {
+            handlPrevious();
+            resetTimer();
+          }}
         />
       </div>
       <div className="flex space-x-1">
@@ -51,7 +62,10 @@ const Carousel = ({ imageUrls, title }) => {
               "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border",
               { "neeto-ui-bg-black": index === currentIndex }
             )}
-            onClick={() => setCurrentIndex(index)}
+            onClick={() => {
+              setCurrentIndex(index);
+              resetTimer();
+            }}
           />
         ))}
       </div>
