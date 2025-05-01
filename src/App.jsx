63be5ea1a0757a710ad2b1a35b9936bf324f5818
Bantyb792 <bantyb792@gmail.com>
2025-05-01 09:18:15 +0530

    Replaced CartItemsContext with useCartItemsStore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import { PageNotFound } from "components/commons";
 import { Redirect, Route, Switch } from "react-router-dom";
 import routes from "routes";
@@ -7,22 +5,15 @@ import routes from "routes";
 import "./App.css";
 import Product from "./components/Product";
 import ProductList from "./components/ProductList";
-import CartItemsContext from "./contexts/CartItemsContext";
 
 // eslint-disable-next-line import/extensions
 
-const App = () => {
-  const [cartItems, setCartItems] = useState([]);
-
-  return (
-    <CartItemsContext.Provider value={[cartItems, setCartItems]}>
-      <Switch>
-        <Route exact component={Product} path={routes.products.show} />
-        <Route exact component={ProductList} path={routes.products.index} />
-        <Redirect exact from={routes.root} to={routes.products.index} />
-        <Route exact component={PageNotFound} path="*" />
-      </Switch>
-    </CartItemsContext.Provider>
-  );
-};
+const App = () => (
+  <Switch>
+    <Route exact component={Product} path={routes.products.show} />
+    <Route exact component={ProductList} path={routes.products.index} />
+    <Redirect exact from={routes.root} to={routes.products.index} />
+    <Route component={PageNotFound} path="*" />
+  </Switch>
+);
 export default App;
