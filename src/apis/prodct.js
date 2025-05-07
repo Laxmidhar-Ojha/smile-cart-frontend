2736e7511ae2740e11f2f963a0062226e85beb35
Bantyb792 <bantyb792@gmail.com>
2025-05-07 21:41:26 +0530

    Created React query custom hook to fetch product and product list

--- src/apis/prodct.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const show = slug => axios.get(`products/${slug}`);
-
-const fetch = params => axios.get("products", { params });
-
-const productsApi = { show, fetch };
-
-export default productsApi;
